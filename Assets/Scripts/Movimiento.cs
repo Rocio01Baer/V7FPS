d25c41c48349d56a88b8d5465996e287bff27f9d
Rocio Ayala <eli19baer@gmail.com>
2022-08-29 14:23:38 -0400

    First commit

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 2f;
+    public void DamageReceived(float damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
